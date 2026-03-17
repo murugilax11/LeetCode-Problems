a3db5e758c7566360f4dd2c54938ce23e9508645
murugilax11 <murugilax11@gmail.com>
2026-03-17 09:39:34 +0530

    Time: 3 ms (35.86%) | Memory: 45.8 MB (47.41%) - LeetSync

--- LinkedList/README/39-combination-sum/combination-sum.java
+++ LinkedList/README/39-combination-sum/combination-sum.java
@@ -2,23 +2,20 @@
     public List<List<Integer>> combinationSum(int[] candidates, int target) 
     {
         List<List<Integer>> res = new ArrayList<>();
-        // Arrays.sort(candidates);
-        backtrack(candidates, target, 0, new ArrayList<>(), res);
+        backtrack(candidates, target,res,new ArrayList<>(),0);
         return res;
     }
-
-    private void backtrack(int[] cand, int target, int start,
-                           List<Integer> cur, List<List<Integer>> res) 
+    private void backtrack(int[] cand, int target, List<List<Integer>> res, List<Integer> temp,int start) 
                            {
         if (target == 0) {
-            res.add(new ArrayList<>(cur));
+            res.add(new ArrayList<>(temp));
             return;
         }
         if (target < 0) return;
         for (int i = start; i < cand.length; i++) {
-            cur.add(cand[i]);
-            backtrack(cand, target - cand[i], i, cur, res);
-            cur.remove(cur.size() - 1);
+            temp.add(cand[i]);
+            backtrack(cand, target - cand[i], res,temp,i);
+            temp.remove(temp.size() - 1);
         }
     }
 }    
