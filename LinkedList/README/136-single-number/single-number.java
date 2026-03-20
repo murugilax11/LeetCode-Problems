8ae1770630c0ef369f48f11e987db2c7d6c47947
murugilax11 <murugilax11@gmail.com>
2026-03-20 21:53:09 +0530

    Time: 11 ms (23.19%) | Memory: 47 MB (60.02%) - LeetSync

---
+++ LinkedList/README/136-single-number/single-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int number=0;
+       for(int i=0; i < nums.length;i++){
+        number ^= nums[i];
+       }
+        return number;
+    }
+}
