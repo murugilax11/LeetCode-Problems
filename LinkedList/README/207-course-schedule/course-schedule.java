1cbfedd2bf6f4dc98483d3f4dec97be2d43cf5eb
murugilax11 <murugilax11@gmail.com>
2026-03-25 11:23:38 +0530

    Time: 8 ms (28.41%) | Memory: 47.4 MB (27.66%) - LeetSync

---
+++ LinkedList/README/207-course-schedule/course-schedule.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+       ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        
+        for(int i =0; i< numCourses;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int[] edge : prerequisites){
+            adj.get(edge[0]).add(edge[1]);
+        }
+        
+        int[] indegree = new int[numCourses];
+        for(int i=0; i< numCourses;i++){
+            for(int n : adj.get(i)){
+                indegree[n]++;
+            }
+        }
+        
+        
+        Queue<Integer> q = new LinkedList<>();
+        for(int i =0; i<numCourses; i++){
+            if(indegree[i] == 0){
+                q.add(i);
+            }
+        }
+        
+        int count =0;
+        while(!q.isEmpty()){
+            int vertex = q.poll();
+            count++;
+            for(int m : adj.get(vertex)){
+                indegree[m]--;
+                if(indegree[m] == 0){
+                    q.add(m);
+                }
+            }
+            
+        }
+        return count == numCourses;  
+    }
+}
