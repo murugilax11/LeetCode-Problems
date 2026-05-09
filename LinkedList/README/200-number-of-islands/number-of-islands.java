05254b437527fa1825ca501a44e56ef8748cd2ae
murugilax11 <murugilax11@gmail.com>
2026-05-09 14:22:47 +0530

    Time: 2 ms (99.82%) | Memory: 52.2 MB (69.41%) - LeetSync

---
+++ LinkedList/README/200-number-of-islands/number-of-islands.java
@@ -0,0 +1,27 @@
+class Solution {
+  void  backtrack(char[][] grid, int row , int col){
+        if(row <0 ||col<0 || row >= grid.length || col >= grid[0].length || grid[row][col] == '0'){
+            return;
+        }
+        grid[row][col] = '0';
+        backtrack(grid,row+1,col);
+        backtrack(grid, row, col+1);
+        backtrack(grid, row-1, col);
+        backtrack(grid, row, col-1);
+    }
+    public int numIslands(char[][] grid) {
+        int count =0;
+        if(grid == null || grid.length == 0) return 0;
+        int row = grid.length;
+        int col = grid[0].length;
+        for(int i =0; i< row; i++){
+            for(int j =0; j <col; j++){
+                if(grid[i][j] =='1'){
+                backtrack(grid,i,j);
+                count++;
+                }
+            }
+        }
+        return count;
+    }
+}
