f3dfcb5cefb65631e83373800f9adbb8b940891d
murugilax11 <murugilax11@gmail.com>
2026-07-14 13:50:34 +0530

    Time: 3 ms (81.47%) | Memory: 43.4 MB (81.97%) - LeetSync

--- LinkedList/README/682-baseball-game/baseball-game.java
+++ LinkedList/README/682-baseball-game/baseball-game.java
@@ -2,9 +2,9 @@ class Solution {
     public int calPoints(String[] operations) {
         Stack<Integer> st = new Stack<>();
         for(String s : operations){
-         if(s.equals("C") && !st.isEmpty()){
+         if(s.equals("C") ){
             st.pop();
-         }else if(s.equals("D") && !st.isEmpty()){
+         }else if(s.equals("D") ){
             st.push(2*st.peek());
          }else if(s.equals("+")){
             int first = st.pop();
